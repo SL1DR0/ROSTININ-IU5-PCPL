809b237747413e0e4a8e778b673ef3546fb182de
unknown <m.rostinin@gmail.com>
2025-12-18 22:45:49 +0300

    lab5 simple bot ver1

---
+++ main.py
@@ -0,0 +1,48 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+TOKEN =
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start_command(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(types.KeyboardButton(text="Привет! 👋"), types.KeyboardButton(text="Как дела? 😊"))
+    builder.row(types.KeyboardButton(text="Создать Inline-кнопки 🔘"))
+
+    await message.answer(
+        "Привет! Я бот с кнопками. Выбери действие на клавиатуре ниже:",
+        reply_markup=builder.as_markup(resize_keyboard=True)
+    )
+
+
+@dp.message(F.text == "Создать Inline-кнопки 🔘")
+async def show_inline(message: types.Message):
+    inline_builder = InlineKeyboardBuilder()
+    inline_builder.row(types.InlineKeyboardButton(
+        text="Перейти на сайт",
+        url="https://google.com")
+    )
+    inline_builder.row(types.InlineKeyboardButton(
+        text="Нажми меня (Callback)",
+        callback_data="button_pressed")
+    )
+
+    await message.answer(
+        "А вот и Inline-кнопки! Они 'прилипают' к сообщению:",
+        reply_markup=inline_builder.as_markup()
+    )
+
+
+async def main():
+    print("Бот запущен...")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
