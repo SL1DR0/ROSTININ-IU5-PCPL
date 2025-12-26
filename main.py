2678f32e6d32eed48a86a5193cc872a846be9e69
unknown <m.rostinin@gmail.com>
2025-12-26 22:28:29 +0300

    minor ref

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-TOKEN ="7524454341:AAHpvUEgbGgIM0aVghsixK9gXcosOqqVpB0"
+TOKEN =""
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
